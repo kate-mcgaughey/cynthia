2c36414e00ab3c714e8e03fdb5b8bea96569c5e1
McGaughey <therealmcgaughey@gmail.com>
2016-04-19 22:10:18 -0600

    Added Audience and Goal

--- milestone-1.php
+++ milestone-1.php
@@ -7,24 +7,27 @@
  */
 
 <!DOCTYPE html>
-<html>
-<body>
+	<html>
 
-<h1>Purpose</h1>
+		<head>
+			<title>Milestone 1</title>
+		</head>
+
+		<body>
+
+			<h2>Purpose</h2>
 
 <?php
-echo "This PWP will be for my friend Cynthia, who is a violinist. She is seeking a site to promote her professional services.";
+echo "This PWP will be for my friend Cynthia, a violinist. She is seeking a site to promote her professional services.";
 ?>
 
+			<h2>Audience</h2>
 
+<?php
+echo "My site is designed for people in the northwest Chicago suburbs and southern Wisconsin, (where Cynthia is from,) who need to hire a violinist. I expect a large focus to be on wedding accompaniment.";
 
+			<h2>Goal</h2>
 
+<?php
+echo "To market Cynthia's services, showcase her playing, and anything else she wants to work in, such as recruit social media followers.";
 
-
-Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
-Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
-To acquire full or part-time employment or an internship.
-To increase sales and/or conversion rates for an existing business.
-To attract freelance clients.
-To serve as a gallery to showcase and promote your work.
-To increase the reach of a new or established social media presence (as measured via analytics tools).
