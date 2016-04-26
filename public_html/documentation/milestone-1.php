5cd4dd6e50ec40938f5d093bb5d0df1fb46a9e12
McGaughey <therealmcgaughey@gmail.com>
2016-04-26 06:24:31 -0600

    Added stylesheet

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,22 +3,25 @@
 
 		<head>
 			<meta charset="UTF-8">
+			<link type="text/css" href="css/style.css" rel="stylesheet" />
 			<title>Personal Website Project - Milestone 1</title>
 		</head>
 
 		<body>
 			<h2>Personal Website Project - Milestone 1</h2>
 
-<p>This PWP will be for my friend Cynthia, a violinist. She is seeking a site to promote her professional services.</p>
+<p>This PWP will be for my cousin's wife Cynthia, a violinist. She is seeking a site to promote her professional services.</p>
 
 			<h2>Audience</h2>
 
-<p>My site is designed for people in the northwest Chicago suburbs and southern Wisconsin, (where Cynthia is from,) who need to hire a violinist. I expect a large focus to be on wedding accompaniment.</p>
+<p>This site is designed for people in the northwest Chicago suburbs and southern Wisconsin, (this is where Cynthia lives,) who need to hire a violinist or small ensemble. I expect a large focus to be on wedding accompaniment.</p>
 
 			<h2>Goal</h2>
 
-<p>To market Cynthia's services, showcase her playing, and anything else she wants to work in, such as recruit social media followers.</p>
+<p>The end goal is to increase Cynthia's bookings at events such as weddings.</p>
 
 			<h2>Persona</h2>
 
 <p>Jane is a 28-year-old woman in Woodstock, IL who is planning her wedding. She's a little overwhelmed by how much there is to do, and has a checklist. Finding a performer for background music seemed to be one of her easier tasks, so she is beginning there. She found Cynthia's site, but doesn't know exactly what she wants performed.</p>
+
+<p></p>
