0902e2e5dbb79ead08f51f370e47b8919bff6b07
McGaughey <therealmcgaughey@gmail.com>
2016-04-29 10:29:44 -0600

    Finished proofreading

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 	</head>
 	<body>
 		<h1>Cynthia Hodgson Music</h1>
-		<p>Thank you for visiting my site! In every event I play, I strive to offer you the best musical experience. I have been a musician for over 20 years. My musical career began at age four, when I began playing violin. I later learned the piano at age six. I have played in various string ensembles since I was eight. My ensemble experience includes the McHenry County Youth Orchestras and the North Park Symphony Orchestra. In addition, I have played for special services and parties for: St. Thomas the Apostle Church, Willow Creek Community Church, Calvary Community Church, the Delavan United Church of Christ, Friends of the Geneva Theatre, Lake Geneva Horticultural Center, and many private parties and weddings.</p>
+		<p>Thank you for visiting my site! For every event I play, I strive to offer you the best musical experience. I have been a musician for over 20 years.</p>
+		<p>My musical career began at age four, when I began playing violin. I later learned the piano at age six. I have played in various string ensembles since I was eight. My ensemble experience includes the McHenry County Youth Orchestras and the North Park Symphony Orchestra. In addition, I have played for special services and parties for: St. Thomas the Apostle Church, Willow Creek Community Church, Calvary Community Church, the Delavan United Church of Christ, Friends of the Geneva Theatre, Lake Geneva Horticultural Center, and many private parties and weddings.</p>
 		<p>As a pianist, I have had the opportunity to play as a soloist and accompanist. My styles of piano playing include classical, jazz, pop/love songs, and ragtime. I am also available to accompany other musicians. Please note that at this time I do request that a piano or keyboard be provided for me.</p>
 		<p>Currently, I am a violinist with the Lake Geneva Symphony. In addition, I am a member of my local church’s praise team, where I serve as violinist, pianist & vocalist for the weekly services. Many blessings to you! I look forward to creating musical memories for your special occasion.</p>
 		<p>Should you be requesting an ensemble, the other musicians are also members of the Lake Geneva Symphony Orchestra or other area ensembles. My ensemble can cover violin, viola, cello, piano, and flute.</p>
 		<img src="" alt="Cynthia hasn't given me her headshot yet"/>
-		<p>Text</p>
+		<p>Pages We Think We Want</p>
 		<ul>
 			<li>About</li>
 			<li>Repertoire</li>
@@ -19,8 +20,6 @@
 			<li>Contact</li>
 		</ul>
 	</body>
-	<HR>
-	
 	<footer>
 		<small>© 2016 by Kate McGaughey, therealmcgaughey@gmail.com</small>
 	</footer>
