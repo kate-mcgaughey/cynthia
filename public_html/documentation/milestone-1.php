5e911e2fb0bb33ab8b0140b93c4ba9bff1647128
McGaughey <therealmcgaughey@gmail.com>
2016-04-29 10:26:18 -0600

    Changed h2's to h3's

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,15 +12,15 @@
 
 <p>This PWP will be for my cousin's wife Cynthia, a violinist. She is seeking a site to promote her professional services.</p>
 
-			<h2>Audience</h2>
+			<h3>Audience</h3>
 
 <p>This site is designed for people in the northwest Chicago suburbs and southern Wisconsin, (this is where Cynthia lives,) who need to hire a violinist or small ensemble. I expect a large focus to be on wedding accompaniment.</p>
 
-			<h2>Goal</h2>
+			<h3>Goal</h3>
 
 <p>The end goal is to increase Cynthia's bookings at events such as weddings.</p>
 
-			<h2>Persona</h2>
+			<h3>Persona</h3>
 
 <p>Jane is a 28-year-old woman in Woodstock, IL who is planning her wedding. She's a little overwhelmed by how much there is to do, and has a checklist. Finding a performer for background music seemed to be one of her easier tasks, so she is beginning there. She found Cynthia's site, but doesn't know exactly what she wants performed.</p>
 
