2213d1fb3f1d71c841152e21a4531b505525c93f
McGaughey <therealmcgaughey@gmail.com>
2016-04-29 11:00:44 -0600

    Updated attitudes, frustrations, and goals

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -22,7 +22,32 @@
 
 			<h3>Persona</h3>
 
-				<p>Jane is a 28-year-old woman in Woodstock, IL who is planning her wedding, which will be in the summer of 2017. She's a little overwhelmed by how much there is to do, and has a checklist. Finding a performer for background music seemed to be one of her easier tasks, so she is beginning there. She found Cynthia's site and thinks Cynthia might be a good fit for her wedding, but doesn't know exactly what she wants performed. She explores the repertoire page, reviews the rates, and decides to inquire about Cynthia's availability.</p>
+				<h4>Name: Jane Downin</h4>
 
+				<h4>Age: 28</h4>
+
+				<h4>Profession:</h4>
+
+					<p>Jane is a registered nurse at Centegra Memorial Medical Center, the local hospital. She works in the heart and vascular ICU. Jane is a deeply caring person who forms close relationships with the patients she sees repeatedly, such as who are in treatment for COPD. Though not humorless, Jane could be described as serious. In her free time, she goes on nature walks with her family and friends, and volunteers at church and for the Animal Humane Society.</p>
+
+				<h4>Technology:</h4>
+
+					<p>Jane uses an old HP desktop computer to browse the web. No one would describer her as a power user. Her mobile phone is an iPhone 5c with a 5GB data allotment.</p>
+
+				<h4>Attitudes and Behavior:</h4>
+
+					<p>Jane is planning her wedding, which will be in the summer of 2017. She's a little overwhelmed by how much there is to do, and has a checklist. Finding a performer for background music seemed to be one of her easier tasks, so she is beginning there. Jane and her fiance are middle-class Americans with student loan debt and the dreams of owning a home and starting a family. They have decided to hold a budget-conscious wedding in order to facilitate these priorities.</p>
+
+				<h4>Frustrations and Needs:</h4>
+
+					<p>Jane wants a performer with skill and class, but struggles with affordability in wedding planning. She also feels like many people in the wedding industry can be very conventional in their thinking, and doesn't want a cookie-cutter ceremony. She needs a performer with a classical repertoire, but knows she's looking for something above and beyond the tired and overused "Ave Maria."</p>
+
+				<h4>Goals:</h4>
+
+					<p>Jane wants to determine whether or not Cynthia is a fit for her wedding. She explores Cynthia's repertoire, reviews her rates, and decides to inquire about Cynthia's availability. She uses the contact form and hopes to sign a contract within a week.</p>
+
+			<h3>Use Case</h3>
+
+				<p></p>
 		</body>
 	</html>
