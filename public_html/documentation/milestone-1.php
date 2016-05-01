94f549f9847fef0370045942b0175662611f96f0
Kate McGaughey <kmcgaughey@cnm.edu>
2016-05-01 16:00:57 -0600

    Added missing purpose header

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,6 +9,8 @@
 		<body>
 			<h2>Personal Website Project - Milestone 1</h2>
 
+			<h3> Purpose</h3>
+			
 				<p>This PWP will be for my cousin's wife Cynthia, a violinist. She is seeking a site to promote her professional services.</p>
 
 			<h3>Audience</h3>
