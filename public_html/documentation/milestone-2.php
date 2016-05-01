500f9581d178fb973c4f28b950886ace9a8f8911
Kate McGaughey <kmcgaughey@cnm.edu>
2016-05-01 16:35:09 -0600

    Added content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,15 +8,33 @@
 
 		<body>
 			<h2>Personal Website Project - Milestone 2</h2>
+			
 			<h3>Sitemap</h3>
+			
 				<p>For this site, I will use only one page with five sections. Navigational links and scrolling both take users from one section to the next. The five sections will be:</p>
 				<ul>About</ul>
 				<ul>Repertoire</ul>
 				<ul>Rates and Details</ul>
 				<ul>Testimonials</ul>
 				<ul>Contact</ul>
-				<p>I intend to learn how to code parallax scrolling for this site, so that the header and possibly background stay the same, while the middle content changes.</p>
+				
+			<p>I intend to learn how to code parallax scrolling for this site, so that the header and possibly background stay the same, while the middle content changes.</p>
 
 			<h3>Content Strategy</h3>
+			
+				<p>The <strong>About</strong> page will contain Cynthia's welcome message and bio:</p>
+					<ul>Thank you for visiting my site! For every event I play, I strive to offer you the best musical experience. I have been a musician for over 20 years. My musical career began at age four, when I began playing violin. I later learned the piano at age six. I have played in various string ensembles since I was eight. My ensemble experience includes the McHenry County Youth Orchestras and the North Park Symphony Orchestra. In addition, I have played for special services and parties for: St. Thomas the Apostle Church, Willow Creek Community Church, Calvary Community Church, the Delavan United Church of Christ, Friends of the Geneva Theatre, Lake Geneva Horticultural Center, and many private parties and weddings.</ul>
+					<ul>As a pianist, I have had the opportunity to play as a soloist and accompanist. My styles of piano playing include classical, jazz, pop/love songs, and ragtime. I am also available to accompany other musicians. Please note that at this time I do request that a piano or keyboard be provided for me. Currently, I am a violinist with the Lake Geneva Symphony. In addition, I am a member of my local church’s praise team, where I serve as violinist, pianist & vocalist for the weekly services. Many blessings to you! I look forward to creating musical memories for your special occasion.</ul>
+					<ul>Should you be requesting an ensemble, the other musicians are also members of the Lake Geneva Symphony Orchestra or other area ensembles. My ensemble can cover violin, viola, cello, piano, and flute.</ul>
+				<p>The Repertoire page will list the pieces Cynthia knows and often plays at special events, plus a message about special requests.</p>
+				<p>The Rates and Details page will display a table of costs involved with hiring Cynthia, plus footnotes such as her policy on late payments.</p>
+				<p>The Testimonials page lists quotes from brides who have hired her, conveying that she does a great job.</p>
+				<p>Cynthia is concerned about a stalker-ish ex-boyfriend, so she would prefer a contact submission form over listing her phone number. I will encourage her to set up an email address for her bookings, such as cynthiahodgsonmusic@gmail.com. The Contact page will have four submission fields:</p>
+					<ul>Name - required</ul>
+					<ul>Phone</ul>
+					<ul>Email</ul>
+					<ul>Message - required</ul>
+				<p>I will create a popup diaglog box saying, "You have not entered a method of contact" if the user does not fill in either the phone or email fields.</p>
+
 		</body>
 	</html>
