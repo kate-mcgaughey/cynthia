67f31c184acf64e532ea429934583012ac27fe08
Kate McGaughey <kmcgaughey@cnm.edu>
2016-05-01 16:43:43 -0600

    Fixed unordered list

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,11 +12,13 @@
 			<h3>Sitemap</h3>
 			
 				<p>For this site, I will use only one page with five sections. Navigational links and scrolling both take users from one section to the next. The five sections will be:</p>
-				<ul>About</ul>
-				<ul>Repertoire</ul>
-				<ul>Rates and Details</ul>
-				<ul>Testimonials</ul>
-				<ul>Contact</ul>
+				<ul>
+					<li>About</li>
+					<li>Repertoire</li>
+					<li>Rates and Details</li>
+					<li>Testimonials</li>
+					<li>Contact</li>
+				</ul>
 				
 			<p>I intend to learn how to code parallax scrolling for this site, so that the header and possibly background stay the same, while the middle content changes.</p>
 
