eff1575d18607cae5d9dba95c26f7f5e8847f133
Kate McGaughey <kmcgaughey@cnm.edu>
2016-05-01 16:15:23 -0600

    Added sitemap

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,3 +5,20 @@
 			<meta charset="utf-8	">
 			<title>Personal Website Project - Milestone 2</title>
 		</head>
+
+		<body>
+			<h2>Personal Website Project - Milestone 2</h2>
+
+			<h3></Sitemap></h3>
+
+				<p>For this site, I will use only one page with five sections. Navigational links and scrolling both take users from one section to the next. The five sections will be:</p>
+
+				<ul>About</ul>
+				<ul>Repertoire</ul>
+				<ul>Rates and Details</ul>
+				<ul>Testimonials</ul>
+				<ul>Contact</ul>
+
+				<p>I intend to learn how to code parallax scrolling for this site, so that the header and possibly background stay the same, while the middle content changes.</p>
+			
+		</body>
