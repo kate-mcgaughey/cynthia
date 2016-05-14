fe9b2717b342cbd55a80b764af9cdfb2c021cbb7
McGaughey <therealmcgaughey@gmail.com>
2016-05-14 15:31:53 -0600

    Added default Bootstrap navbar

--- index.php
+++ index.php
@@ -4,8 +4,62 @@
 		<meta charset="utf-8">
 		<title>Cynthia Hodgson Music - Violin performance and accompaniment in Woodstock, IL - Wedding Violinist</title>
 	</head>
+
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Brand</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">One more separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
 	<body>
-		<h1>Cynthia Hodgson Music</h1>
 		<p>Thank you for visiting my site! For every event I play, I strive to offer you the best musical experience. I have been a musician for over 20 years.</p>
 		<p>My musical career began at age four, when I began playing violin. I later learned the piano at age six. I have played in various string ensembles since I was eight. My ensemble experience includes the McHenry County Youth Orchestras and the North Park Symphony Orchestra. In addition, I have played for special services and parties for: St. Thomas the Apostle Church, Willow Creek Community Church, Calvary Community Church, the Delavan United Church of Christ, Friends of the Geneva Theatre, Lake Geneva Horticultural Center, and many private parties and weddings. </p>
 		<img src="" alt="Cynthia hasn't given me her headshot yet"/>
