73923bb188d3268fde563c6e2735c513ff4037ae
McGaughey <therealmcgaughey@gmail.com>
2016-06-01 22:14:15 -0600

    Added Bootstrap CDN links

--- index.php
+++ index.php
@@ -2,75 +2,66 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href=.....fill this in later....>
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- OUR CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<!-- ADD YOUR JAVASCRIPT/JQUERY DOWN HERE -->
+
 		<title>Cynthia Hodgson Music - Violin performance and accompaniment in Woodstock, IL - Wedding Violinist</title>
 	</head>
 
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
+
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Brand</a>
+				<a class="navbar-brand" href="#">Cynthia Hodgson Music</a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Link</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-					</li>
-				</ul>
-				<form class="navbar-form navbar-left" role="search">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Search">
-					</div>
-					<button type="submit" class="btn btn-default">Submit</button>
-				</form>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Link</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-						</ul>
-					</li>
+					<li class="active"><a href="#">Home</a></li>
+					<li class="active"><a href="#">Repertoire</a></li>
+					<li class="active"><a href="#">Rates and Terms</a></li>
+					<li class="active"><a href="#">Testimonials</a></li>
+					<li class="active"><a href="#">Contact</a></li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
 
 	<body>
-		<p>Thank you for visiting my site! For every event I play, I strive to offer you the best musical experience. I have been a musician for over 20 years.</p>
+		<p>Thank you for visiting my site! I am a performer with over 20 years experience. For every event I play, I strive to offer you the best musical experience.</p>
 		<p>My musical career began at age four, when I began playing violin. I later learned the piano at age six. I have played in various string ensembles since I was eight. My ensemble experience includes the McHenry County Youth Orchestras and the North Park Symphony Orchestra. In addition, I have played for special services and parties for: St. Thomas the Apostle Church, Willow Creek Community Church, Calvary Community Church, the Delavan United Church of Christ, Friends of the Geneva Theatre, Lake Geneva Horticultural Center, and many private parties and weddings. </p>
 		<img src="" alt="Cynthia hasn't given me her headshot yet"/>
-		<p>Pages We Think We Want</p>
-		<ul>
-			<li>About</li>
-			<li>Repertoire</li>
-			<li>Rates and Terms</li>
-			<li>Testimonials</li>
-			<li>Contact</li>
-		</ul>
+		<p>"Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything."  - Plato</p>
 	</body>
 	<footer>
 		<small>© 2016 by Kate McGaughey, therealmcgaughey@gmail.com</small>
